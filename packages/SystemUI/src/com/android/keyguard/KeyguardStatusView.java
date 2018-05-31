1532513cd09db5af30d18affb0fb8236acf6fb07
Shreyansh Lodha <slodha96@gmail.com>
2018-05-31 19:39:06 +0000

    lockscreen : Add small size style [1/2]
    Change-Id: I9917a1d6c12f71161d1f74ccb021d89154f3339e
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -297,7 +297,10 @@ public class KeyguardStatusView extends GridLayout implements
         } else if (mClockSelection == 5) {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
-        } else {
+        } else if (mClockSelection == 6) {
+	    mClockView.setFormat12Hour(Html.fromHtml("<strong>h:mm</strong>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk:mm</strong>"));
+	} else {
             mClockView.setFormat12Hour("hh\nmm");
             mClockView.setFormat24Hour("kk\nmm");
         }
@@ -444,6 +447,7 @@ public class KeyguardStatusView extends GridLayout implements
                 mSpideyClockView.setVisibility(View.GONE);
                 break;
             case 1: // digital (bold)
+	    case 6: // digital (small)
                 mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
                 mAnalogClockView.setVisibility(View.GONE);
                 mDeadPoolClockView.setVisibility(View.GONE);
@@ -462,11 +466,7 @@ public class KeyguardStatusView extends GridLayout implements
                 mSpideyClockView.setVisibility(View.GONE);
                 break;
             case 4: // sammy
-                mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
-                mAnalogClockView.setVisibility(View.GONE);
-                mDeadPoolClockView.setVisibility(View.GONE);
-                break;
-            case 5: // sammy (bold)
+	    case 5: // sammy (bold)
                 mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE : View.GONE) : View.VISIBLE);
                 mAnalogClockView.setVisibility(View.GONE);
                 mDeadPoolClockView.setVisibility(View.GONE);
@@ -532,6 +532,15 @@ public class KeyguardStatusView extends GridLayout implements
                 Settings.System.LOCKSCREEN_DATE_SELECTION, 0, UserHandle.USER_CURRENT);
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) mKeyguardStatusArea.getLayoutParams();
+
+	if (mClockSelection == 6) {
+	    mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimensionPixelSize(R.dimen.widget_small_font_size));
+	} else {
+	    mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
+	}
+
         switch (mClockSelection) {
             case 0: // default digital
             default:
@@ -543,6 +552,7 @@ public class KeyguardStatusView extends GridLayout implements
                 mSpideyClockView.unregisterReceiver();
                 break;
             case 1: // digital (bold)
+	    case 6: // digital (smal)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
                 mClockView.setGravity(Gravity.CENTER);
@@ -563,12 +573,6 @@ public class KeyguardStatusView extends GridLayout implements
                 mSpideyClockView.unregisterReceiver();
                 break;
             case 4: // sammy
-                params.addRule(RelativeLayout.BELOW, R.id.clock_view);
-                mClockView.setSingleLine(false);
-                mClockView.setGravity(Gravity.CENTER);
-                mAnalogClockView.unregisterReceiver();
-                mDeadPoolClockView.unregisterReceiver();
-                break;
             case 5: // sammy (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
