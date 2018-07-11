bedb019861453147da03cf96bcaa46f0b569c0ed
phenomenall <venkysaihot@gmail.com>
2018-07-11 04:46:22 +0000

    Revert "Base: Add some more accents [3/3]"
    This reverts commit cedb8b135869fcd1608ca7e198e515fd813801fd.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4894,7 +4894,6 @@ public final class Settings {
          * 1 - force light
          * 2 - force dark
          * 3 - force black
-         * 4 - force extendedUI
          * @hide
          */
         public static final String SYSTEM_THEME_STYLE = "system_theme_style";
